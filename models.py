a91912b66eb0e08663d2dec4a67e4ba22f9f7875
Shalini Singh <99865685+Shalinikashyap0108@users.noreply.github.com>
2025-07-17 12:53:18 +0530

    Update models.py

--- models.py
+++ models.py
@@ -6,8 +6,8 @@ def train_model(model_name, X_train, y_train):
     if model_name == "Linear Regression":
         model = LinearRegression()
     elif model_name == "Decision Tree":
-        model = DecisionTree(random_state=42)
+        model = DecisionTree(max_depth=7, random_state=42)
     elif model_name == "Random Forest":
-        model = RandomForestRegressor(random_state=42)
+        model = RandomForestRegressor(n_estimators = 100, max_depth=7, random_state=42)
     model.fit(X_train, y_train)
     return model
