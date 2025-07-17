2eb1c2adef80c938fec4469218fc4911f5cb3af8
Shalini Singh <99865685+Shalinikashyap0108@users.noreply.github.com>
2025-07-17 13:05:33 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+# models.py
+
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
@@ -6,8 +8,12 @@ def train_model(model_name, X_train, y_train, params):
     if model_name == "Linear Regression":
         model = LinearRegression()
     elif model_name == "Decision Tree":
-        model = DecisionTree(max_depth=params.get("max depth", None), randome_state=42)
+        model = DecisionTreeRegressor(max_depth=params.get("max_depth", None), random_state=42)
     elif model_name == "Random Forest":
-        model = RandomForestRegressor(n_estimators=params.get("n_estimators", 100), max_depth=params.get("max depth", None), random_state=42)
+        model = RandomForestRegressor(
+            n_estimators=params.get("n_estimators", 100),
+            max_depth=params.get("max_depth", None),
+            random_state=42
+        )
     model.fit(X_train, y_train)
     return model
